fa3baec5704ed6789eea1c0591b7285bfefb99f0
mcvinding <mikkel.vinding@gmail.com>
2020-10-26 13:11:30 +0100

    fooof analysis done

--- scripts/PDbb2_08_FOOOF.py
+++ scripts/PDbb2_08_FOOOF.py
@@ -6,285 +6,108 @@ FOOOF analysis of powerspectra in PDbb2 project.
 """
 # Import fooof functions for creating spectra and managing parameters
 import numpy as np
-import scipy.io
 import pandas
-import fooof
 import scipy.io as sio
-from fooof.analysis import get_band_peak, get_band_peak_group
 import matplotlib.pyplot as plt
+import os.path as op
+from os import mkdir
+import sys
+sys.path.append('/home/mikkel/PD_longrest/scripts')
+from PDbb2_SETUP import subjects, meg_path
 
-# Import some fooof plotting functions
-from fooof.plts.spectra import plot_spectrum, plot_spectra
-from fooof.plts.spectra import plot_spectrum_shading, plot_spectra_shading
-
+# Import fooof functions
 from fooof import FOOOF
 from fooof import FOOOFGroup
-
-
-from fooof.plts.spectra import plot_spectrum, plot_spectra
-from fooof.plts.spectra import plot_spectrum_shading, plot_spectra_shading
 from fooof.bands import Bands
-from fooof import FOOOF
+from fooof.analysis import get_band_peak_fg #get_band_peak_fm
+#from fooof.plts.spectra import plot_spectrum, plot_spectra
+#from fooof.plts.spectra import plot_spectrum_shading, plot_spectra_shading
+from fooof.plts.periodic import plot_peak_fits, plot_peak_params
+from fooof.plts.aperiodic import plot_aperiodic_params, plot_aperiodic_fits
 
 #%% SETTINGS
 # Define frequency bands of interest
-theta_band = [4, 8]
-alpha_band = [8, 13]
-beta_band = [13, 30]
-
-groups = ['ctrl_dat1','ptns_dat1','ctrl_dat2','ptns_dat2']
-
-# Constrain analysis
-max_n_peaks = 8
-
-# Define frequency range across which to model the spectrum
-freq_range = [1, 40]
-        
-#%% Read data
-dat = scipy.io.loadmat('/home/mikkel/PD_motor/rest_ec/groupanalysis/PSD_data.mat')
-
-#%% COPYPASTA
 bands = Bands({'delta' : [1, 4],
                'theta' : [4, 8],
                'alpha' : [8, 13],
                'beta'  : [13, 30],
                'gamma' : [30, 45]})
 
-shade_cols = ['#e8dc35', '#46b870', '#1882d9', '#a218d9', '#e60026']
+# Constrain analysis
+max_n_peaks = 8
+peak_wlim   = [0.75, 12]
+freq_range  = [0.5, 45]
               
-              
-psd = sio.loadmat(psdfname)['psd'][0]
-freqs = sio.loadmat(psdfname)['freqs'][0]
+#%% Load and collect data
+all_psd = np.zeros((len(subjects), 137))
+all_freqs = np.zeros((len(subjects), 137))
 
-    # Initialize a FOOOF object
-fm = FOOOF(max_n_peaks=10, peak_threshold=2, peak_width_limits = [.75, 12], aperiodic_mode='fixed', min_peak_height=0.025)
+for ii, subj in enumerate(subjects):
 
-# Set the frequency range to fit the model
-#freq_range = [0.1, 45]
-
-# Report: fit the model, print the resulting parameters, and plot the reconstruction
-fm.report(freqs, psd, freq_range)
-#fm.plot()
-
-plot_spectrum_shading(freqs, psd, log_powers=True, linewidth=3, shades=bands.definitions, shade_colors=shade_cols)
-plt.xlim(freq_range);
-plt.title('Band-by-Band', t_settings);
-
-# Plot the power spectra differences
-plot_spectrum_shading(freqs, fm.fooofed_spectrum_,
-                     log_powers=False, linewidth=3,
-                     shades=bands.definitions, shade_colors=shade_cols)
-plt.xlim(f_range);
-plt.title('Band-by-Band - Flattened', t_settings);
-
-
-
-#%% Do FOOOF
-alpha_dict = dict()
-beta_dict = dict()
-theta_dict = dict()
-aper_dict = dict()
-rsq_dict = dict()
-
-for jj in groups:
+    hemi = 'lh'
+    subj_path   = op.join(meg_path, subj)
+    psdfname    = op.join(subj_path, subj+'-ts-psd'+hemi+'.mat')
+    figdir      = op.join(subj_path, 'plots')
+    if not op.exists(figdir):
+        mkdir(figdir)
     
-    #Init.
-    alpha_mat = np.zeros([len(dat[jj]),3])
-    beta_mat = np.zeros([len(dat[jj]),3])
-    theta_mat = np.zeros([len(dat[jj]),3])
-    aper_mat = np.zeros([len(dat[jj]),2])
-    
-    rsq_mat = np.zeros([len(dat[jj]),1])
-    
-    for ii in range(len(dat[jj])):
-        print(ii)
-        
-        freq = dat[jj][ii][0][0][0][2][0,]
-        psd = dat[jj][ii][0][0][0][3][0,]
-    
-        ## PLOT
-        ##plot_spectrum(fs, psd[0,])
-        #plot_spectrum(freq, psd_mat)
-        #plot_spectrum_shading(freq, psd_mat, [13, 30], log_powers=True)
-    
-        # Initialize FOOOF object
-        fm = FOOOF(max_n_peaks=max_n_peaks, min_peak_height=0.025, peak_width_limits = [.75, 12], aperiodic_mode='fixed')
-    
-        # Fit model
-        fm.fit(freq, psd, freq_range)
-        
-        # Model the power spectrum with FOOOF, and print out a report
-#        fm.print_results()
-#        fm.plot()
-    
-        # Extract band-specific oscillations from the FOOOF model
-        beta_mat[ii,] = get_band_peak(fm.peak_params_, beta_band)
-        alpha_mat[ii,] = get_band_peak(fm.peak_params_, alpha_band)
-        theta_mat[ii,] = get_band_peak(fm.peak_params_, theta_band)
-        aper_mat[ii,] = fm.get_params('aperiodic_params')
-        rsq_mat[ii] = fm.get_params('r_squared')
-        
-    beta_dict[jj] = beta_mat.copy()
-    alpha_dict[jj] = alpha_mat.copy()
-    theta_dict[jj] = theta_mat.copy()
-    aper_dict[jj] = aper_mat.copy()
-    rsq_dict[jj] = rsq_mat.copy()
+    # Load data
+    all_psd[ii,:] = sio.loadmat(psdfname)['psd'][0]
+    all_freqs[ii,:] = sio.loadmat(psdfname)['freqs'][0]
 
-#%% Arrange dataframe and export
+    # Individual FOOOF analysis  (for inspection)
+    fm = FOOOF(max_n_peaks=max_n_peaks, peak_threshold=2, peak_width_limits=peak_wlim, 
+               aperiodic_mode='fixed', min_peak_height=0.05)
 
-df_beta = pandas.DataFrame()
-df_alpha = pandas.DataFrame()
-df_theta = pandas.DataFrame()
-df_aper = pandas.DataFrame()
+    # Report: fit the model, print the resulting parameters, and plot the reconstruction
+    fm.report(all_freqs[ii,], all_psd[ii,], freq_range)
+    plt.title(subj)
+    plt.savefig(op.join(figdir, 'fooof_psd.jpg'))
+    plt.close()
 
-for con in beta_dict:
-    temp = beta_dict[con].copy()
-    print(con)
-    
-    session = np.ones(len(temp)) if con[-1]=='1' else np.ones(len(temp))*2
-    group = np.ones(len(temp)) if con[:4]=='ptns' else np.ones(len(temp))*2
-    
-    d_temp = {'peak_freq': temp[:,0],
-              'peak_pow': temp[:,1],
-              'peak_width': temp[:,2],
-              'group': group,
-              'session': session}
-    db_temp = pandas.DataFrame(d_temp)
-    df_beta = df_beta.append(db_temp)
+#%% FOOOF analysis
+fg = FOOOFGroup(max_n_peaks=max_n_peaks, peak_threshold=2, peak_width_limits=peak_wlim, 
+                aperiodic_mode='fixed', min_peak_height=0.05)
+fg.fit(all_freqs[0], all_psd)
 
-for con in alpha_dict:
-    temp = alpha_dict[con].copy()
-    print(con)
-    
-    session = np.ones(len(temp)) if con[-1]=='1' else np.ones(len(temp))*2
-    group = np.ones(len(temp)) if con[:4]=='ptns' else np.ones(len(temp))*2
-    
-    d_temp = {'peak_freq': temp[:,0],
-              'peak_pow': temp[:,1],
-              'peak_width': temp[:,2],
-              'group': group,
-              'session': session}
-    db_temp = pandas.DataFrame(d_temp)
-    df_alpha = df_alpha.append(db_temp)
-    
-    
-for con in theta_dict:
-    temp = theta_dict[con].copy()
-    print(con)
-    
-    session = np.ones(len(temp)) if con[-1]=='1' else np.ones(len(temp))*2
-    group = np.ones(len(temp)) if con[:4]=='ptns' else np.ones(len(temp))*2
-    
-    d_temp = {'peak_freq': temp[:,0],
-              'peak_pow': temp[:,1],
-              'peak_width': temp[:,2],
-              'group': group,
-              'session': session}
-    db_temp = pandas.DataFrame(d_temp)
-    df_theta = df_theta.append(db_temp)
-    
-for con in aper_dict:
-    temp = aper_dict[con].copy()
-    print(con)
-    
-    session = np.ones(len(temp)) if con[-1]=='1' else np.ones(len(temp))*2
-    group = np.ones(len(temp)) if con[:4]=='ptns' else np.ones(len(temp))*2
-    
-    d_temp = {'intercept': temp[:,0],
-              'slope': temp[:,1],
-              'group': group,
-              'session': session}
-    db_temp = pandas.DataFrame(d_temp)
-    df_aper = df_aper.append(db_temp)    
+# Get parameters
+aper  = fg.get_params('aperiodic_params')
+delta = get_band_peak_fg(fg, bands.delta)
+theta = get_band_peak_fg(fg, bands.theta)
+alpha = get_band_peak_fg(fg, bands.alpha)
+beta  = get_band_peak_fg(fg, bands.beta)
+gamma = get_band_peak_fg(fg, bands.gamma)
 
-# Export    
-df_beta.to_csv('/home/mikkel/PD_motor/rest_ec/groupanalysis/df_beta.csv', index=False, sep=';')
-df_alpha.to_csv('/home/mikkel/PD_motor/rest_ec/groupanalysis/df_alpha.csv', index=False, sep=';')
-df_theta.to_csv('/home/mikkel/PD_motor/rest_ec/groupanalysis/df_theta.csv', index=False, sep=';')
-df_aper.to_csv('/home/mikkel/PD_motor/rest_ec/groupanalysis/df_aper.csv', index=False, sep=';')
+fg.plot()
 
-#%% Inspect
-for p in rsq_dict:
-    plt.figure(1)
-    plt.hist(rsq_dict[p], alpha=.5, label=p); plt.title('R-squared')
-plt.legend()    
-
-for p in aper_dict:
-    plt.figure(2)
-    plt.hist(aper_dict[p][:,0], alpha=.5, edgecolor='k', label=p); plt.title('Intercept')
-plt.legend()    
-
-for p in aper_dict:    
-    plt.figure(3)
-    plt.hist(aper_dict[p][:,1], alpha=.5, edgecolor='k', label=p); plt.title('slope')
-plt.legend()    
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Below here still in progress: move to somewhere else 
-
-#%% Plot
-for jj in groups:
-    plt.figure(1)
-    plt.hist(beta_dict[jj][:,1], 10); plt.title('Max peak pow')
-    plt.figure(2)
-    plt.hist(beta_dict[jj][:,0], 10); plt.title('Max peak freq')
-    plt.figure(3)
-    plt.hist(beta_dict[jj][:,2], 10); plt.title('Max peak width')
-
-for jj in groups:
-    plt.figure(4)
-    plt.hist(theta_dict[jj][:,1], 10); plt.title('Max peak pow')
-    plt.figure(5)
-    plt.hist(theta_dict[jj][:,0], 10); plt.title('Max peak freq')
-    plt.figure(6)
-    plt.hist(theta_dict[jj][:,2], 10); plt.title('Max peak width')
-
-for jj in groups:
-    plt.figure(7)
-    plt.hist(alpha_dict[jj][:,1], 10); plt.title('Max peak pow')
-    plt.figure(8)
-    plt.hist(alpha_dict[jj][:,0], 10); plt.title('Max peak freq')
-    plt.figure(9)
-    plt.hist(alpha_dict[jj][:,2], 10); plt.title('Max peak width')    
-    
-
-#%% Combined figure
-colors = [[0,.5,1], [0,0,1], [1,.5,0], [1,0,0] ]
-dat_b = [beta_dict['ctrl_dat1'][:,0], beta_dict['ctrl_dat2'][:,0], beta_dict['ptns_dat1'][:,0], beta_dict['ptns_dat2'][:,0]]
-dat_a = [alpha_dict['ctrl_dat1'][:,0], alpha_dict['ctrl_dat2'][:,0], alpha_dict['ptns_dat1'][:,0], alpha_dict['ptns_dat2'][:,0]]
-dat_t = [theta_dict['ctrl_dat1'][:,0], theta_dict['ctrl_dat2'][:,0], theta_dict['ptns_dat1'][:,0], theta_dict['ptns_dat2'][:,0]]
-
-bins = np.linspace(4, 31, 28)    
-
-plt.figure()
-plt.hist(dat_b, bins, label='x', edgecolor='k', color=colors, alpha=1, ls='-')
-plt.hist(dat_a, bins, label='x', edgecolor='k', color=colors, alpha=1, ls='--')
-plt.hist(dat_t, bins, label='x', edgecolor='k', color=colors, alpha=1, ls=':')
-
-#%%
-plt.hist(beta_dict['ptns_dat1'][:,0], bins, alpha=0.5, label='x', edgecolor='k',hatch='/', facecolor='b', histtype='stacked')
-plt.hist(beta_dict['ptns_dat2'][:,0], bins, alpha=0.5, label='x', edgecolor='k', facecolor='b', histtype='stacked')
-plt.hist(beta_dict['ctrl_dat1'][:,0], bins, alpha=0.5, label='x', edgecolor='k',hatch='/', facecolor='r', histtype='stacked')
-plt.hist(beta_dict['ctrl_dat2'][:,0], bins, alpha=0.5, label='x', edgecolor='k', facecolor='r', histtype='stacked')
+plot_peak_params(alpha, freq_range=bands.alpha)
+plot_peak_params(beta, freq_range=bands.beta)
+plot_aperiodic_params(aper)
 
 
+#%% Export
+df_dct = {'subj':subjects,
+      'a_intercept': aper[:,0],
+      'a_slope': aper[:,1],
+      'delta_cf': delta[:,0],
+      'delta_pw': delta[:,1],
+      'delta_bw': delta[:,2],
+      'theta_cf': theta[:,0],
+      'theta_pw': theta[:,1],
+      'theta_bw': theta[:,2],
+      'alpha_cf': alpha[:,0],
+      'alpha_pw': alpha[:,1],
+      'alpha_bw': alpha[:,2],
+      'beta_cf': beta[:,0],
+      'beta_pw': beta[:,1],
+      'beta_bw': beta[:,2],
+      'gamma_cf': gamma[:,0],
+      'gamma_pw': gamma[:,1],
+      'gamma_bw': gamma[:,2]}
 
-plt.hist(theta_dict['ctrl_dat1'][:,0], bins, alpha=0.5, label='x', edgecolor='b')
-plt.hist(alpha_dict['ctrl_dat1'][:,0], bins, alpha=0.5, label='x', edgecolor='b')
-plt.hist(beta_dict['ctrl_dat2'][:,0], bins, alpha=0.5, label='x', edgecolor='b')
-plt.hist(theta_dict['ctrl_dat2'][:,0], bins, alpha=0.5, label='x', edgecolor='b')
-plt.hist(alpha_dict['ctrl_dat2'][:,0], bins, alpha=0.5, label='x', edgecolor='b')
+df = pandas.DataFrame(df_dct)
 
-pyplot.hist(y, bins, alpha=0.5, label='y')
-pyplot.legend(loc='upper right')
-pyplot.show()
+# save to csv    
+df.to_csv('/home/mikkel/PD_longrest/groupanalysis/fooof_df.csv', index=False, sep=';')
 
-    
-    
-    
-#%%     
-# Create and save out a report summarizing the results across the group of power spectra
-fg.save_report()
-
-# Save out FOOOF results for further analysis later
-fg.save(file_name='fooof_group_results', save_results=True)
+#END
